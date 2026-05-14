f46c66f27da2f8bada2e0dec7787c05442b0d34f
azhar-khan3 <142772149+azhar-khan3@users.noreply.github.com>
2026-05-14 08:54:48 +0300

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,7 @@ const fileupload = require('express-fileupload');
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
 }
 bootstrap();

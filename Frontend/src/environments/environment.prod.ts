7ea9989bbe6d1adfe7bc4f8335932b09bc480d5c
azhar-khan3 <142772149+azhar-khan3@users.noreply.github.com>
2026-05-14 09:39:16 +0300

    Update environment.prod.ts

--- Frontend/src/environments/environment.prod.ts
+++ Frontend/src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: "https://shopcart-mercer-backend.onrender.com",
+  productApi: "https://shopcart-mercer-backend.onrender.com/product",
+  productFilterApi: "https://shopcart-mercer-backend.onrender.com/product/filter"
 };
